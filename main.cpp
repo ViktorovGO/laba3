a853ed61fc4c994c31f84dce08ea9b2be11b7282
ViktorovGO <62133391+ViktorovGO@users.noreply.github.com>
2020-04-10 14:41:29 +0300

    Add files via upload
    добавили svg

--- main.cpp
+++ main.cpp
@@ -75,6 +75,59 @@ void show_histogram_text(const vector<size_t>& bins)
         cout << '\n';
     }
 }
+void svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end()
+{
+    cout << "</svg>\n";
+}
+void svg_text(double left,double baseline , string text)
+{
+    cout<< "<text x='" << left << "' y='"<<baseline<<"'>'"<<text<<"'</text>";
+}
+void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black")
+{
+    cout<<"<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
+}
+
+
+void show_histogram_svg(const vector<size_t>& bins)
+{   const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top=0;
+    size_t t=0;
+    for(size_t bin:bins)
+    {
+    const double bin_width =BLOCK_WIDTH*bin;
+    svg_text(TEXT_LEFT,top+TEXT_BASELINE,to_string(bin));
+    svg_rect(TEXT_WIDTH,top,bin_width,BIN_HEIGHT);
+    if(t==1)
+    {
+        svg_rect(TEXT_WIDTH,top,bin_width,BIN_HEIGHT,"red");
+    }
+    if(t==2)
+    {
+        svg_rect(TEXT_WIDTH,top,bin_width,BIN_HEIGHT,"blue","#aaffaa");
+    }
+    top+=BIN_HEIGHT;
+    t++;
+    }
+    svg_end();
+}
 
 int main() {
     // ¬вод данных
@@ -90,6 +143,6 @@ int main() {
 
     // ќбработка данных
     const auto bins =make_histogram(bin_count,numbers);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 }
