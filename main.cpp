44463ffe8d54cdead9df43f91edb2b127c678104
ViktorovGO <62133391+ViktorovGO@users.noreply.github.com>
2020-04-10 12:31:38 +0300

    Add files via upload
    code:программа через функции

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<double> input_numbers(size_t count)
+{
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++)
+    {
+        cin >> result[i];
+    }
+    return result;
+}
+void find_minmax(const vector<double>& numbers,double &min,double &max)
+{
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers)
+    {
+        if (number < min) {
+            min = number;
+        }
+        if (number > max) {
+            max = number;
+        }
+    }
+}
+vector<size_t> make_histogram(size_t count,const vector<double>& numbers)
+{   double min=0;
+    double max=0;
+    find_minmax(numbers,min,max);
+    vector<size_t> bins(count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * count);
+        if (bin == count)
+        {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+void show_histogram_text(const vector<size_t>& bins)
+{
+    // Вывод данных
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
+
+int main() {
+    // Ввод данных
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+    // Обработка данных
+    const auto bins =make_histogram(bin_count,numbers);
+    show_histogram_text(bins);
+    return 0;
+}
