0c77ec36b69ffbc2495428bc9cdba05e543ae4c9
ViktorovGO <62133391+ViktorovGO@users.noreply.github.com>
2020-04-10 15:12:00 +0300

    Add files via upload
    провели тесты ,внесли исправления

---
+++ histogram.cpp
@@ -0,0 +1,18 @@
+#include "histogram.h"
+void find_minmax(const vector<double>& numbers,double &min,double &max)
+{   int q=numbers.size();
+    if(q!=0)
+    {
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers)
+    {
+        if (number < min) {
+            min = number;
+        }
+        if (number > max) {
+            max = number;
+        }
+    }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include "histogram.h"
 using namespace std;
 vector<double> input_numbers(size_t count)
 {
@@ -10,20 +10,6 @@ vector<double> input_numbers(size_t count)
         cin >> result[i];
     }
     return result;
-}
-void find_minmax(const vector<double>& numbers,double &min,double &max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
 }
 vector<size_t> make_histogram(size_t count,const vector<double>& numbers)
 {   double min=0;
