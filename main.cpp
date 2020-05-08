1749e72a72fc41725629e1af6c696d262cca5075
ViktorovGO <62133391+ViktorovGO@users.noreply.github.com>
2020-05-08 10:31:23 +0300

    potok

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "svg.h"
 
 using namespace std;
-vector<double> input_numbers(size_t count)
+vector<double> input_numbers(istream&in,size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -38,7 +38,7 @@ int main()
     cin >> number_count;
 
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin,number_count);
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
